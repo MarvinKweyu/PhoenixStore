c44b325e4aa62dcc0acc0e1b6a193d62e6e55a56
Marvin Kweyu <mkweyu1@gmail.com>
2022-07-09 17:58:01 +0300

    feat: add admin layout and incl flash resp messages for response

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -2,6 +2,11 @@ defmodule HelloWeb.PageController do
   use HelloWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    conn
+    |> put_status(:created)
+    |> put_flash(:info, "Welcome to Pheonix")
+    |> put_flash(:error, "Let's pretend we have an error")
+    |> put_root_layout("admin.html")
+    |> render(:index)
   end
 end
